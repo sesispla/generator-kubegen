c854bf3cb839580f1f92fcfb1ce6d64fe670cdaa
Sergio Sisternes <sesispla@outlook.com>
2017-07-05 20:25:58 +0200

    Additional test specification for svc.yml

--- tests/app.js
+++ tests/app.js
@@ -11,10 +11,10 @@ describe('Replication Controller with no Ingress scenarios', function () {
     beforeEach(function () {
         return helpers.run(path.join(__dirname, '../generators/app'))
             .withPrompts({
-                name: "nginx",
-                namespace: "default",
-                podControllerType: "Replication Controller",
-                image: "nginx",
+                name: 'nginx',
+                namespace: 'default',
+                podControllerType: 'Replication Controller',
+                image: 'nginx',
                 replicas: 1,
                 containerPort: 80,
                 servicePort: 8080,
@@ -22,22 +22,30 @@ describe('Replication Controller with no Ingress scenarios', function () {
             });
     });    
 
-    it('File rc.yml is generated and properly filled in', function () {
+    it('File rc.yml is generated and properly are filled in', function () {
         assert.file(['rc.yml']);
         var rc = yaml.load('rc.yml');
         assert.equal(rc.apiVersion, 'v1');
         assert.equal(rc.kind, 'ReplicationController');
-        assert.equal(rc.metadata.name, "nginx");
-        assert.equal(rc.metadata.namespace, "default");
+        assert.equal(rc.metadata.name, 'nginx');
+        assert.equal(rc.metadata.namespace, 'default');
         assert.equal(rc.spec.replicas, 1);
-        assert.equal(rc.spec.selector.app, "nginx");
-        assert.equal(rc.spec.template.metadata.labels.app, "nginx");
-        assert.equal(rc.spec.template.spec.containers[0].name, "nginx");
-        assert.equal(rc.spec.template.spec.containers[0].image, "nginx");
+        assert.equal(rc.spec.selector.app, 'nginx');
+        assert.equal(rc.spec.template.metadata.labels.app, 'nginx');
+        assert.equal(rc.spec.template.spec.containers[0].name, 'nginx');
+        assert.equal(rc.spec.template.spec.containers[0].image, 'nginx');
     });
 
-    it('File svc.yml is generated and properly filled in', function () {
+    it('File svc.yml is generated and properly are filled in', function () {
         assert.file(['svc.yml']);
+        var svc = yaml.load('svc.yml');
+        assert.equal(svc.apiVersion, 'v1');
+        assert.equal(svc.kind, 'Service');
+        assert.equal(svc.metadata.name, 'nginx');
+        assert.equal(svc.metadata.namespace, 'default');
+        assert.equal(svc.spec.ports[0].port, 8080);
+        assert.equal(svc.spec.ports[0].targetPort, 80);
+        assert.equal(svc.spec.selector.app, 'nginx');
     });
 
     it('File ing.yml is not generated', function () {
