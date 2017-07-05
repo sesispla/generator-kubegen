f612eec4d9ea7995d551d3e9601825d12b10edd0
Sergio Sisternes <sesispla@outlook.com>
2017-07-05 20:19:16 +0200

    Tests name refactoring

--- tests/app.js
+++ tests/app.js
@@ -6,25 +6,23 @@ var assert = require('yeoman-assert');
 var path = require('path');
 var yaml = require('yamljs');
 
-beforeEach(function () {
-    return helpers.run(path.join(__dirname, '../generators/app'))
-        .withPrompts({
-            name: "nginx",
-            namespace: "default",
-            podControllerType: "Replication Controller",
-            image: "nginx",
-            replicas: 1,
-            containerPort: 80,
-            servicePort: 8080,
-            shouldExpose: false
-        });
-});
+describe('Replication Controller with no Ingress scenarios', function () {
 
+    beforeEach(function () {
+        return helpers.run(path.join(__dirname, '../generators/app'))
+            .withPrompts({
+                name: "nginx",
+                namespace: "default",
+                podControllerType: "Replication Controller",
+                image: "nginx",
+                replicas: 1,
+                containerPort: 80,
+                servicePort: 8080,
+                shouldExpose: false
+            });
+    });    
 
-
-describe('Replication Controller scenarios', function () {
-    
-    it('generates file rc.yml', function () {
+    it('File rc.yml is generated and properly filled in', function () {
         assert.file(['rc.yml']);
         var rc = yaml.load('rc.yml');
         assert.equal(rc.apiVersion, 'v1');
@@ -38,12 +36,16 @@ describe('Replication Controller scenarios', function () {
         assert.equal(rc.spec.template.spec.containers[0].image, "nginx");
     });
 
-    it('generates file svc.yml', function () {
+    it('File svc.yml is generated and properly filled in', function () {
         assert.file(['svc.yml']);
     });
 
-    it('do not generates file ing.yml', function () {
+    it('File ing.yml is not generated', function () {
         assert.noFile(['ing.yml']);
     });
+
+    it('File deployment.yml is not generated', function () {
+        assert.noFile(['deployment.yml']);
+    });    
 
 });
